248c621cd2707d0009da69ad8a426fa328514455
Dev_Faruq <olatoyesefaruq@gmail.com>
2026-07-11 05:57:20 +0100

    feat: add new social platforms

--- lib/platforms.tsx
+++ lib/platforms.tsx
@@ -1,4 +1,18 @@
 import React from "react";
+import { 
+  FaGithub, 
+  FaFacebook, 
+  FaTwitter, 
+  FaInstagram, 
+  FaLinkedin, 
+  FaYoutube, 
+  FaTiktok, 
+  FaDiscord, 
+  FaSlack, 
+  FaGitlab, 
+  FaDev 
+} from "react-icons/fa";
+import { SiHashnode } from "react-icons/si";
 
 export type PlatformConfig = {
   id: string;
@@ -18,11 +32,7 @@ export const platforms: PlatformConfig[] = [
     bg: "bg-[#1A1A1A] hover:bg-[#333333] text-white",
     pattern: /^https:\/\/(www\.)?github\.com\/.+$/,
     placeholder: "https://github.com/username",
-    icon: (
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-        <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
-      </svg>
-    )
+    icon: <FaGithub size={16} />
   },
   {
     id: "facebook",
@@ -31,11 +41,7 @@ export const platforms: PlatformConfig[] = [
     bg: "bg-[#1877F2] hover:bg-[#3b5998] text-white",
     pattern: /^https:\/\/(www\.)?facebook\.com\/.+$/,
     placeholder: "https://facebook.com/username",
-    icon: (
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-        <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
-      </svg>
-    )
+    icon: <FaFacebook size={16} />
   },
   {
     id: "twitter",
@@ -44,11 +50,7 @@ export const platforms: PlatformConfig[] = [
     bg: "bg-[#1D9BF0] hover:bg-[#007cc4] text-white",
     pattern: /^https:\/\/(www\.)?(x|twitter)\.com\/.+$/,
     placeholder: "https://x.com/username",
-    icon: (
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-        <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.6.75zm-.86 13.028h1.36L4.323 2.145H2.865l8.875 11.633z"/>
-      </svg>
-    )
+    icon: <FaTwitter size={16} />
   },
   {
     id: "instagram",
@@ -57,11 +59,7 @@ export const platforms: PlatformConfig[] = [
     bg: "bg-gradient-to-r from-[#833AB4] via-[#FD1D1D] to-[#F56040] hover:brightness-110 text-white",
     pattern: /^https:\/\/(www\.)?instagram\.com\/.+$/,
     placeholder: "https://instagram.com/username",
-    icon: (
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-        <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.917 3.917 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.21.51.5 1.049.92 1.467.418.419.957.708 1.468.908.5.198 1.08.333 1.94.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.909-1.396.197-.5.331-1.082.37-1.936.04-.852.05-1.123.05-3.297 0-2.172-.01-2.444-.048-3.298c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0H8zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"/>
-      </svg>
-    )
+    icon: <FaInstagram size={16} />
   },
   {
     id: "linkedin",
@@ -70,10 +68,69 @@ export const platforms: PlatformConfig[] = [
     bg: "bg-[#0A66C2] hover:bg-[#004182] text-white",
     pattern: /^https:\/\/(www\.)?linkedin\.com\/(in|pub)\/.+$/,
     placeholder: "https://linkedin.com/in/username",
-    icon: (
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-        <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm10.99 8.212V9.359c0-2.167-1.157-3.178-2.702-3.178-1.246 0-1.794.686-2.105 1.178h.017V6.169H7.497c.032.678 0 7.225 0 7.225h2.401V9.752c0-.401.029-.803.147-1.089.324-.803 1.06-1.636 2.292-1.636 1.616 0 2.261 1.233 2.261 3.042v4.18H14.77z"/>
-      </svg>
-    )
+    icon: <FaLinkedin size={16} />
+  },
+  {
+    id: "youtube",
+    name: "YouTube",
+    color: "#FF0000",
+    bg: "bg-[#FF0000] hover:bg-[#cc0000] text-white",
+    pattern: /^https:\/\/(www\.)?youtube\.com\/.+$/,
+    placeholder: "https://youtube.com/@username",
+    icon: <FaYoutube size={16} />
+  },
+  {
+    id: "tiktok",
+    name: "TikTok",
+    color: "#010101",
+    bg: "bg-[#010101] hover:bg-[#1a1a1a] text-white",
+    pattern: /^https:\/\/(www\.)?tiktok\.com\/.+$/,
+    placeholder: "https://tiktok.com/@username",
+    icon: <FaTiktok size={16} />
+  },
+  {
+    id: "discord",
+    name: "Discord",
+    color: "#5865F2",
+    bg: "bg-[#5865F2] hover:bg-[#4752c4] text-white",
+    pattern: /^https:\/\/(www\.)?(discord\.com|discord\.gg)\/.+$/,
+    placeholder: "https://discord.gg/invitecode",
+    icon: <FaDiscord size={16} />
+  },
+  {
+    id: "slack",
+    name: "Slack",
+    color: "#4A154B",
+    bg: "bg-[#4A154B] hover:bg-[#3b103c] text-white",
+    pattern: /^https:\/\/[a-z0-9-]+\.slack\.com\/.+$/,
+    placeholder: "https://workspace.slack.com",
+    icon: <FaSlack size={16} />
+  },
+  {
+    id: "gitlab",
+    name: "GitLab",
+    color: "#FC6D26",
+    bg: "bg-[#FC6D26] hover:bg-[#e24e04] text-white",
+    pattern: /^https:\/\/(www\.)?gitlab\.com\/.+$/,
+    placeholder: "https://gitlab.com/username",
+    icon: <FaGitlab size={16} />
+  },
+  {
+    id: "devto",
+    name: "Dev.to",
+    color: "#0A0A0A",
+    bg: "bg-[#0A0A0A] hover:bg-[#222222] text-white",
+    pattern: /^https:\/\/(www\.)?dev\.to\/.+$/,
+    placeholder: "https://dev.to/username",
+    icon: <FaDev size={16} />
+  },
+  {
+    id: "hashnode",
+    name: "Hashnode",
+    color: "#2962FF",
+    bg: "bg-[#2962FF] hover:bg-[#0039cb] text-white",
+    pattern: /^https:\/\/([a-z0-9-]+\.)?hashnode\.dev\/.+$|^https:\/\/hashnode\.com\/@.+$/,
+    placeholder: "https://hashnode.dev/@username",
+    icon: <SiHashnode size={16} />
   }
 ];
